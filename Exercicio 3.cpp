1298671a3c129637ebcdd716f8c727b0a97fb730
Laura <133364744+laurajhagsuUGAVGHA@users.noreply.github.com>
2024-04-19 15:42:38 -0300

    Initial commit

---
+++ Exercicio 3.cpp
@@ -0,0 +1,72 @@
+
+
+
+//Primeiro 
+/*#include <iostream>
+using namespace std;
+
+int main() {
+
+int numero1;
+int numero2;
+
+cout << "Informe o primeiro número: ";
+cin >> numero1;
+
+cout << "Informe o segundo número";
+cin >> numero2;
+
+if (numero1 > numero2) {
+	cout << "O primeiro número é maior" << endl;
+}
+else if (numero2 > numero1) {
+	cout << "O segundo número é maior " << endl;
+}
+else {
+	cout << "Os números são iguais" << endl;
+}
+
+	
+}*/
+
+//SEGUNDO 
+/*#include <iostream>
+ using namespace std;
+
+int main() {
+	int quantidadeProduto;
+
+	cout << "Informar a quantidade de Produtos";
+	cin >> quantidadeProduto;
+	
+	if (quantidadeProduto >= 100) {
+		cout << "Quantidade suficiente no estoque" << endl;
+	}
+	else if (quantidadeProduto >= 50) {
+		cout << "Atenção ao estoque" << endl;
+	}
+	else {
+		cout << " Faça um novo pedido e reabasteça o estoque" << endl;
+	}
+}*/
+//TERCEIRO
+/*#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int numero;
+
+	cout << "Informe um número" << endl;
+	cin >> numero; 
+
+	if (numero > 0) {
+		cout << "Número positivo" << endl;
+
+	}
+	else if (numero < 0) {
+		cout << "Número negativo" << endl;
+	}
+
+	return 0;
+}*/
